1b8e19ab4995ccc6c1031e8eddf4f69edbfc082e
achu-bsl <asharudeenalikt@gmail.com>
2023-10-01 21:47:46 +0530

    Enhace validation with ZodValidationPipe

---
+++ src/pipes/zodValidation.pipe.ts
@@ -0,0 +1,18 @@
+import {
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { ZodType } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodType<unknown>) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value);
+    } catch (err) {
+      throw new BadRequestException('Vadlidation failed.');
+    }
+    return value;
+  }
+}
