6520121f705026b54caeda073433e434691fd37f
achu-bsl <asharudeenalikt@gmail.com>
2023-10-03 10:25:13 +0530

    Protected login route with using login user guard

---
+++ src/modules/auth/guards/login-user.guard.ts
@@ -0,0 +1,32 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { PrismaService } from 'src/modules/prisma/prisma.service';
+import { LoginUerDto } from 'src/modules/user/dto/login-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class LoginUserGuard implements CanActivate {
+  constructor(private readonly prisma: PrismaService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const { user_email, password } = request.body as LoginUerDto;
+    if (!user_email || !password)
+      throw new BadRequestException('Email and password are mandatory');
+
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { user_email },
+      });
+      if (user == null || !(await bcrypt.compare(password, user.password)))
+        throw new Error('Incorrect email or password');
+      return true;
+    } catch (err) {
+      throw new BadRequestException(err.message);
+    }
+  }
+}
