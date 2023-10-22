f18cae8685265e752811213ffb23d8e3207fd527
achu-bsl <asharudeenalikt@gmail.com>
2023-10-22 20:01:08 +0530

    Implement editUser and changeTwoFactorAuthState controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,10 +2,13 @@ import {
   Body,
   Controller,
   Get,
+  Param,
   Patch,
   Req,
+  UploadedFile,
   UseGuards,
   UseInterceptors,
+  UsePipes,
 } from '@nestjs/common';
 
 import { UserService } from './user.service';
@@ -18,6 +21,9 @@ import { editUserSchema } from './schema/zod.schema';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { extname } from 'path';
 import { diskStorage } from 'multer';
+import { FileUploadTransformPipe } from './pipe/fileupload.pipe';
+import { UsernameAvailableGuard } from './guard/UsernameAvailable.guard';
+import { ChangeTwoFactorAuthPipe } from './pipe/change-two-factor-auth.pipe';
 
 @Controller('user')
 export class UserController {
@@ -30,5 +36,45 @@ export class UserController {
     return this.userService.getUser(user_id);
   }
 
- 
+  @Patch()
+  @UseGuards(UserAuthGuard)
+  @UseGuards(UsernameAvailableGuard)
+  @UseInterceptors(
+    FileInterceptor('user_profile', {
+      storage: diskStorage({
+        destination: (req, file, cb) => {
+          cb(null, './uploads');
+        },
+        filename: (req, file, callback) => {
+          const uniqueSuffix = Date.now() + -Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  async editUser(
+    @Req() request: Request,
+    @Body(new ZodValidationPipe(editUserSchema)) updatedUserData: EdituserDto,
+    @UploadedFile(new FileUploadTransformPipe()) user_profile: string
+  ) {
+    const { user_id } = request.user as UserPayloadInterface;
+    if(user_profile) {
+      updatedUserData.user_profile = user_profile
+    }
+    return this.userService.editUser(user_id, updatedUserData);
+  }
+
+
+  @Patch('two-factor-auth/:new_state')
+  @UseGuards(UserAuthGuard)
+  @UsePipes(ChangeTwoFactorAuthPipe)
+  async changeTwoFactorAuthState (
+    @Req() request: Request,
+    @Param('new_state') new_state: 'enable' | 'disable'
+  ) {
+    const { user_id } = request.user as UserPayloadInterface;
+    return this.userService.toggleTwoFactorAuth(user_id, new_state === 'enable')
+  }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,11 +2,14 @@ import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { PrismaModule } from "../prisma/prisma.module";
+import { MulterModule } from "@nestjs/platform-express";
 
 
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, MulterModule.register({
+        dest: "./uploads"
+    })],
     controllers: [UserController],
     providers: [UserService],
     exports: [UserService]
