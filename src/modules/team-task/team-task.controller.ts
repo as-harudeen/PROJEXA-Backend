e1536258032bf5cb7779deac04648b673d528243
achu-bsl <asharudeenalikt@gmail.com>
2023-11-07 20:33:04 +0530

    Implement api route for update team task

--- src/modules/team-task/team-task.controller.ts
+++ src/modules/team-task/team-task.controller.ts
@@ -14,6 +14,8 @@ import { Request } from 'express';
 import { UserPayloadInterface } from '../auth/interface';
 import { GetTaskParamDto } from './dto/get-task.dto';
 import { TransformGetTaskInterceptor } from './interceptor/transform-get-task.interceptor';
+import { UpdateTaskStatusParamDto } from './dto/update-task-status.dto';
+import { TeamTaskStatus } from '@prisma/client';
 
 
 @Controller('team/:team_id/project/:project_id/task')
@@ -31,6 +33,21 @@ export class TeamTaskController {
     return this.teamTaskService.getTeamProjectTask({
       ...param,
       user_id,
+    });
+  }
+
+  @Patch(':task_id/status')
+  @UseGuards(UserAuthGuard)
+  async updateTaskStatus(
+    @Req() request: Request,
+    @Param() param: UpdateTaskStatusParamDto,
+    @Body('new_task_status') new_task_status: TeamTaskStatus,
+  ) {
+    const { user_id } = request.user as UserPayloadInterface;
+    return this.teamTaskService.updateTaskStatus({
+      ...param,
+      user_id,
+      new_task_status,
     });
   }
 
--- src/modules/team-task/team-task.service.ts
+++ src/modules/team-task/team-task.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { GetTaskDto } from './dto/get-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { UpdateTeamTaskDto } from './dto/update-team-task.dto';
 
 @Injectable()
 export class TeamTaskService {
@@ -47,6 +49,58 @@ export class TeamTaskService {
           task_status: true,
         },
       });
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async updateTaskStatus({
+    team_id,
+    project_id,
+    task_id,
+    user_id,
+    new_task_status,
+  }: UpdateTaskStatusDto) {
+    try {
+      const {
+        team_projects: { team_projects_id },
+      } = await this.prisma.team.findUniqueOrThrow({
+        where: {
+          team_id,
+          OR: [
+            { team_admins_id: { has: user_id } },
+            { team_members_id: { has: user_id } },
+          ],
+        },
+        select: {
+          team_projects: {
+            select: {
+              team_projects_id: true,
+            },
+          },
+        },
+      });
+
+      await this.prisma.teamIndividualProject.findUniqueOrThrow({
+        where: {
+          team_project_id: project_id,
+          team_projects_id,
+        },
+      });
+
+      await this.prisma.teamProjectTask.update({
+        where: {
+          task_id,
+          assigned_to_user_id: user_id,
+          team_project_id: project_id,
+          task_status: { not: new_task_status },
+        },
+        data: {
+          task_status: new_task_status,
+        },
+      });
+
+      return 'Task status updated successfully';
     } catch (err) {
       throw new InternalServerErrorException(err.message);
     }
