b5ae448085d2fcc0e9c3e948d054a5d6a4104153
achu-bsl <asharudeenalikt@gmail.com>
2023-11-30 13:15:06 +0530

    enabled serve static files in prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TeamModule } from './modules/team/team.module';
 import { ProjectModule } from './modules/project/project.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'client', "dist"),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
